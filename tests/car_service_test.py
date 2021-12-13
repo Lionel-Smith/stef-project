26350d1f977443315527234fb6be1070103bd818
Lionel Smith <LSmith@nib-bahamas.com>
2021-12-13 16:06:08 -0500

    tests

---
+++ tests/car_service_test.py
@@ -0,0 +1,26 @@
+from tests.mock_car_wash_repo import MockCarWashRepo
+from werkzeug.exceptions import BadRequest
+from src.stef_project.DTO.car_DTO import carDTO
+from src.stef_project.enums.exceptionMessages import UserExceptions
+from src.stef_project.car_service import CarService
+import unittest
+
+class MockCarWashService(unittest.TestCase):
+    def __init__(self) -> None:
+        self.car_service = CarService()
+        self.car_service.repo= MockCarWashRepo()
+        self.license_plate= "ad4569"
+        self.car_color = "green"
+        self.is_dirty = False
+        self.hours_parked = 12
+        self.price = 0
+    
+    #add requests
+    def test_add_car(self):
+        self.license_plate ="AF5687"
+        self.car_color = "Beige"
+        self.is_dirty = True
+        self.hours_parked = 8
+        self.price = 0
+        self.assertIsNot(self.car_service.add_car(self.license_plate, self.car_color, self.is_dirty, self.hours_parked, self.price), None)
+
---
+++ tests/mock_car_wash_repo.py
@@ -0,0 +1,45 @@
+from tests.car_service_test import MockCarWashService
+from src.stef_project.DTO.car_DTO import carDTO
+from src.models.car_model import CarModel, db
+from werkzeug.exceptions import BadRequest
+from src.stef_project.enums.exceptionMessages import UserExceptions
+import sqlite3
+
+
+class MockCarWashRepo:
+    def __init__(self) -> None:
+        self.db = db
+
+    def add_car(self, license_plate, car_color, is_dirty, hrs_parked, Price) ->None:
+        new_car = CarModel(license_plate= license_plate,
+        is_dirty= is_dirty, 
+        car_color= car_color,
+        hours_parked= hrs_parked, 
+        Price=Price
+        )
+        db.session.add(new_car) 
+        db.session.commit()
+        car : CarModel = CarModel.query.filter(CarModel.license_plate==license_plate).first()
+        car_DTO= carDTO(car.license_plate,car.car_color,car.is_dirty,car.hrs_parked,car.price)
+        return car_DTO
+
+        
+    def update_car(self, license_plate, car_color, is_dirty, hours_parked,  price):
+        car = CarModel.query.filter(CarModel.license_plate==license_plate).first()
+        car.is_dirty= is_dirty
+        car.car_color= car_color
+        car.hrs_parked= hours_parked 
+        car.price= price
+        db.session.add(car)
+        db.session.commit()
+        car_DTO= carDTO(car.license_plate,car.car_color,car.is_dirty,car.hrs_parked,car.price)
+        return car_DTO
+
+    def remove_car(license_plate) ->None:
+        car = CarModel.query.filter(CarModel.license_plate==license_plate).delete()
+        db.session.commit()
+    
+    def find_car(self, license_plate) ->carDTO:
+        car : CarModel = CarModel.query.filter(CarModel.license_plate==license_plate).first()
+        car_DTO= carDTO(car.license_plate,car.car_color,car.is_dirty,car.hrs_parked,car.price)
+        return car_DTO
